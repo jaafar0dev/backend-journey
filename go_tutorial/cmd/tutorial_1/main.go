59eb54931280b5decc48ca476ee4af365bb3dc2e
jaafar0dev <jaafar.dev@protonmail.com>
2026-01-22 22:00:37 +0100

    Publish Github

--- go_tutorial/cmd/tutorial_1/main.go
+++ go_tutorial/cmd/tutorial_1/main.go
@@ -1,25 +1,36 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
 func main() {
 
-	bands := []string{"Metallica", "Megadeth", "Slayer", "Anthrax"}
+	// var intArr [3]int
+	// intArr[1] = 10
 
-	fmt.Println(len(bands))
+	// fmt.Println(&intArr[1])
+
+	// var intArr []int = []int{1, 2, 3}
+
+	// intArr = append(intArr, 8)
+
+	// fmt.Println(len(intArr))
+	// fmt.Println(cap(intArr))
+
+	// var intArr2 []int32 = make([]int32, 3, 8)
+	 
+
 }
 
-func nameSurname(firstname string, lastname string) (string, int, error) {
+// func nameSurname(firstname string, lastname string) (string, int, error) {
 
-	fullname := firstname + " " + lastname
-	var err error
+// 	fullname := firstname + " " + lastname
+// 	var err error
 
-	if firstname == "lol" || lastname == "lol" {
-		err = errors.New("invalid name")
-	}
-	age := len(fullname)
-	return fullname, age, err
-}
+// 	if firstname == "lol" || lastname == "lol" {
+// 		err = errors.New("invalid name")
+// 	}
+// 	age := len(fullname)
+// 	return fullname, age, err
+// }
